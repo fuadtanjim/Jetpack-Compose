109d5b3035e2f67177d483348608bdae9edd8f7c
fuad <tanjim977@gmail.com>
2024-05-17 19:10:02 +0600

    Initial commit

---
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.example.jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.jetpackcompose.ui.theme.JetpackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackComposeTheme {
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var names by remember {
+                    mutableStateOf(listOf<String>())
+                }
+               Column(
+                   modifier = Modifier
+                       .fillMaxSize()
+                       .padding(16.dp)
+               ) {
+                   Row(
+                       modifier = Modifier.fillMaxWidth()
+                   ) {
+                       OutlinedTextField(
+                           value = name,
+                           onValueChange = { text ->
+                               name = text
+                           },
+                           modifier = Modifier.weight(1f)
+                       )
+                       Spacer(modifier = Modifier.width(16.dp))
+                       Button(onClick = {
+                           if(name.isNotBlank()) {
+                               names = names + name
+                               name = ""
+                           }
+                       }) {
+                           Text(
+                               text = "Add"
+                           )
+                       }
+                   }
+                   NameList(names = names)
+               }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun NameList(
+    names: List<String>,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(modifier) {
+        items(names) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
+        }
+    }
+}
